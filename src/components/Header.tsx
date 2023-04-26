a43b8b307e5f33f98ed17191d059414fac7d3af7
Pham Phuc <phamphuc05071998@gmail.com>
2023-04-26 16:22:52 +0700

    inital project

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Header.module.css' 
+import logo from './../assets/img/logo-white.png'
+function Header() {
+  return (
+   <header className={styles.header}>
+    <div className={styles.logoBox}>
+        <img src={logo} alt='logo' className={styles.logo}/>
+    </div>
+    <div className={styles.textBox}>
+        
+    <h1 className={styles.headerPrimary}><span className={styles.headerPrimaryMain}>
+    Outdoors  </span>
+    <span className={styles.headerPrimarySub}>is where life happens</span></h1>
+    </div>
+   </header>
+  )
+}
+
+export default Header
