c2c886d2ffd6dbff6b8678ed1cc1601a3a1eec5f
Pham Phuc <phamphuc05071998@gmail.com>
2023-04-26 17:14:42 +0700

    Add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import styles from './Header.module.css' 
-import logo from './../assets/img/logo-white.png'
+import React from "react";
+import styles from "./Header.module.css";
+import logo from "./../assets/img/logo-white.png";
 function Header() {
   return (
-   <header className={styles.header}>
-    <div className={styles.logoBox}>
-        <img src={logo} alt='logo' className={styles.logo}/>
-    </div>
-    <div className={styles.textBox}>
-        
-    <h1 className={styles.headerPrimary}><span className={styles.headerPrimaryMain}>
-    Outdoors  </span>
-    <span className={styles.headerPrimarySub}>is where life happens</span></h1>
-    </div>
-   </header>
-  )
+    <header className={styles.header}>
+      <div className={styles.logoBox}>
+        <img src={logo} alt="logo" className={styles.logo} />
+      </div>
+      <div className={styles.textBox}>
+        <h1 className={styles.headerPrimary}>
+          <span className={styles.headerPrimaryMain}>Outdoors </span>
+          <span className={styles.headerPrimarySub}>is where life happens</span>
+        </h1>
+        <a href="#" className={`${styles.btn} ${styles.btnWhite} ${styles.btnAnimated}`}>Discover our tour </a>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
