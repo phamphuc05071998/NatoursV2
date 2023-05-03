b7601345047962544c74c303edfd403415aef0b2
Pham Phuc <phamphuc05071998@gmail.com>
2023-05-03 21:11:08 +0700

    Add Navigation

--- src/components/Home/Navigation/Navigation.tsx
+++ src/components/Home/Navigation/Navigation.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./Navigation.module.scss";
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <div className={styles.navigation}>
+      <input
+        className={styles.navigationCheckBox}
+        type="checkbox"
+        id="navi-toggle"
+      ></input>
+      <label htmlFor="navi-toggle" className={styles.navigationButton}><span className={styles.navigationIcon}>&nbsp;</span></label>
+      <div className={styles.navigationBackground}>&nbsp;</div>
+      <nav className={styles.navigationNav}>
+        <ul className={styles.navigationList}>
+          <li className={styles.navigationItem}>
+            <a href="#" className={styles.navigationLink}><span >01</span> About Natous</a>
+          </li>
+          <li className={styles.navigationItem}>
+            <a href="#" className={styles.navigationLink}> <span >02</span>Your benifit</a>
+          </li>
+          <li className={styles.navigationItem}>
+            <a href="#" className={styles.navigationLink}> <span >03</span>Popular tours</a>
+          </li>
+          <li className={styles.navigationItem}>
+            <a href="#" className={styles.navigationLink}> <span >04</span>Stories</a>
+          </li>
+          <li className={styles.navigationItem}>
+            <a href="#" className={styles.navigationLink}><span >05</span>Book now</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default Navigation
+export default Navigation;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,10 +6,12 @@ import { Tour } from '../components/Home/Tour/Tour'
 import Stories from '../components/Home/Stories/Stories'
 import Booking from '../components/Home/Booking/Booking'
 import Footer from '../components/Home/Footer/Footer'
+import Navigation from '../components/Home/Navigation/Navigation'
 // import TestGrid from '../components/testGrid/TestGrid'
 function Home() {
   return (
     <>
+    <Navigation></Navigation>
     <Header/>
     {/* <TestGrid></TestGrid> */}
     <main>
